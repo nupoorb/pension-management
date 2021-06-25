68a3c58b946e2febc43d71b850a678edf7510369
VS <910179@cognizant.com>
2021-06-25 10:09:12 +0530

    first commit

---
+++ PensionerDetail/src/main/java/com/pensionmanagement/pensionerdetail/PensionerDetailApplication.java
@@ -0,0 +1,13 @@
+package com.pensionmanagement.pensionerdetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionerDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionerDetailApplication.class, args);
+	}
+
+}
---
+++ PensionerDetail/src/test/java/com/pensionmanagement/pensionerdetail/PensionerDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pensionmanagement.pensionerdetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionerDetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
