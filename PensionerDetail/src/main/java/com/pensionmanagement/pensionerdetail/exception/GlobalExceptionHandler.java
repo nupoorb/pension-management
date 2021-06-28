d83cd0e4c6753f3ef5e6ad4f09646bf809494a30
VS <910179@cognizant.com>
2021-06-28 18:22:40 +0530

    updated

--- PensionerDetail/src/main/java/com/pensionmanagement/pensionerdetail/exception/GlobalExceptionHandler.java
+++ PensionerDetail/src/main/java/com/pensionmanagement/pensionerdetail/exception/GlobalExceptionHandler.java
@@ -1,9 +1,23 @@
 package com.pensionmanagement.pensionerdetail.exception;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+@ControllerAdvice
 public class GlobalExceptionHandler {
 	
+	@ExceptionHandler(NotFoundException.class)
 	
+		public ResponseEntity<String> invalidAdhaarNumber(NotFoundException ex)
+		{
+			return new ResponseEntity<>(ex.toString(),HttpStatus.NOT_ACCEPTABLE);
+		}
+	 @ExceptionHandler(InvalidTokenException.class)
+		public ResponseEntity<String> invalidPensionerDetails(InvalidTokenException ex)
+		{
+			return new ResponseEntity<>(ex.toString(),HttpStatus.UNAUTHORIZED);
+		}
 	}
 
 
---
+++ PensionerDetail/src/main/java/com/pensionmanagement/pensionerdetail/service/PensionerDetailServiceImpl.java
@@ -0,0 +1,52 @@
+package com.pensionmanagement.pensionerdetail.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.pensionmanagement.pensionerdetail.exception.NotFoundException;
+import com.pensionmanagement.pensionerdetail.model.PensionerDetail;
+import com.pensionmanagement.pensionerdetail.util.DateUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class PensionerDetailServiceImpl implements PensionerDetailService{
+
+	
+	private Map<Long, PensionerDetail> pensionDetails;
+	
+	
+	@Override
+	public PensionerDetail getById(long aadharnumber) throws NumberFormatException, IOException, ParseException, NotFoundException   {
+		log.info("getById method called");
+
+		String line = "";
+		pensionDetails = new HashMap<>();
+		BufferedReader bufferRead = new BufferedReader(
+				new InputStreamReader(this.getClass().getResourceAsStream("/pensionerData.csv")));
+		while ((line = bufferRead.readLine()) != null)
+				{
+					String[] persondetails = line.split(",");
+					PensionerDetail pensioner = new PensionerDetail(Long.parseLong(persondetails[0]),persondetails[1],
+							DateUtil.parseDate(persondetails[2]), persondetails[3], Double.parseDouble(persondetails[4]),
+							Double.parseDouble(persondetails[5]), persondetails[6], persondetails[7],
+							Long.parseLong(persondetails[8]), persondetails[9]);
+					pensionDetails.put(Long.parseLong(persondetails[0]), pensioner);
+				}
+		bufferRead.close();
+		if (pensionDetails.containsKey(aadharnumber)) {
+			return pensionDetails.get(aadharnumber);
+		} else {
+			throw new NotFoundException("The Adhaar Number is Unregistered");
+		}
+		
+		
+	}
+	}
