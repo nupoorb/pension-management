d7a35ca61cdb0f2712c71f2829e05ab2b431fdf8
VS <910179@cognizant.com>
2021-06-28 18:57:56 +0530

    updated

--- PensionerDetail/src/main/java/com/pensionmanagement/pensionerdetail/PensionerDetailApplication.java
+++ PensionerDetail/src/main/java/com/pensionmanagement/pensionerdetail/PensionerDetailApplication.java
@@ -2,8 +2,10 @@ package com.pensionmanagement.pensionerdetail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PensionerDetailApplication {
 
 	public static void main(String[] args) {
