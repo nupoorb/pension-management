98ba3674045abca72a56921e600d1d341bf4c665
Achyuth KV <overboardfreak@gmail.com>
2021-06-28 18:54:16 +0530

    Updated

--- Portal/src/main/java/com/pensionmanagement/portal/PortalApplication.java
+++ Portal/src/main/java/com/pensionmanagement/portal/PortalApplication.java
@@ -2,8 +2,10 @@ package com.pensionmanagement.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PortalApplication {
 
 	public static void main(String[] args) {
