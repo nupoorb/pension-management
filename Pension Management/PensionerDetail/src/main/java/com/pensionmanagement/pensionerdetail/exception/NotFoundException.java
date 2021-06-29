1fcf633ea30fb3447e218cd12830c0e34ff4d4a5
Nupoor <nupoor6499@gmail.com>
2021-06-29 19:56:27 +0530

    Updated 29 June 2021

---
+++ Pension Management/PensionerDetail/src/main/java/com/pensionmanagement/pensionerdetail/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.pensionmanagement.pensionerdetail.exception;
+
+public class NotFoundException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	public NotFoundException(String msg)
+	{
+		super(msg);
+	}
+	
+
+}
---
+++ Pension Management/Portal/src/main/java/com/pensionmanagement/portal/clients/AuthServiceClient.java
@@ -0,0 +1,21 @@
+package com.pensionmanagement.portal.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.pensionmanagement.portal.exception.TokenException;
+import com.pensionmanagement.portal.model.UserLoginCredential;
+
+@FeignClient(name = "authorization-service", url = "${auth.valid}")
+public interface AuthServiceClient {
+
+	@PostMapping("/login")
+	ResponseEntity<UserLoginCredential> authenticateUserAndGetDetails(@RequestBody UserLoginCredential login);
+
+	@PostMapping("/validate")
+	public boolean validateToken(@RequestHeader("Authorization") String token)throws TokenException;
+
+}
